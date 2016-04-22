0eb375f39d9d8c5737492f1b68180af219e32c95
Sharma, Rahul <rahul.sharma@rakuten.com>
2016-04-21 18:25:28 -0700

    Added bean validation and event interception for performing business logic before persisting to DB

---
+++ cassandraRestFeatures/src/main/java/com/example/repositories/PersonRepository.java
@@ -0,0 +1,1 @@
+package com.example.repositories;import com.example.entities.Person;import org.springframework.data.repository.CrudRepository;/** * Created by rahul.sharma on 4/20/16. */public interface PersonRepository extends CrudRepository<Person,String> {}
